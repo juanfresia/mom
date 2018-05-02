640bae8f3e558b52415d9ea03170380127cf91c7
juanfresia <juanmanuelfresia@gmail.com>
2018-05-02 02:09:11 -0300

    Log with colors ?!
    Signed-off-by: juanfresia <juanmanuelfresia@gmail.com>

--- log.c
+++ log.c
@@ -4,7 +4,6 @@
 #define _GNU_SOURCE
 #include "log.h"
 
-
 #include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -15,12 +14,23 @@ extern char *program_invocation_short_name;
 void log_printf(char* msg, ... ) {
     setbuf(stdout, NULL);
     int pid = getpid();
+
+    // If stdout is a tty - change color
+    if (isatty(1)) {
+        printf("\x1b[1;38;5;%dm", ((pid % 20) * 2 + 1));
+    }
+
     va_list args;
     va_start(args, msg);
 
     printf("[%d %s] - ", pid, program_invocation_short_name);
     vprintf(msg, args);
     va_end(args);
+
+    // If stdout is a tty - restore color
+    if (isatty(1)) {
+        printf("\x1b[39m");
+    }
 }
 
 void log_perror(char* msg) {
